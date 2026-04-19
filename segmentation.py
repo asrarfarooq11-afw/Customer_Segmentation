f17ee31c5ec5e370e20855dee00dbbea004fce2b
asrarfarooq11-afw <asrarfarooq11@gmail.com>
2026-04-19 18:42:36 +0530

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -49,7 +49,7 @@ segment_map = joblib.load("segment_map.pkl")
 
 # ─── INPUTS ──────────────────────────────
 age = st.number_input("Age", min_value=18, max_value=100, value=35)
-income = st.number_input("Income", min_value=0, max_value=200000, value=5000)
+income = st.number_input("Income(in dollars /Annual)", min_value=0, max_value=200000, value=5000)
 total_spending = st.number_input("Total Spending", min_value=0, max_value=50000, value=1000)
 
 num_web_purchases = st.number_input("Web Purchases", min_value=0, max_value=100, value=10)
